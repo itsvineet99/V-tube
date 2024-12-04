d46cd4a0b4532359999700b6abc88b5ecdee52ba
Vineet Dorikar <vineetdorikar06@gmail.com>
2024-12-04 12:40:10 +0530

    start with controllers

--- src/utils/asyncHandeler.js
+++ src/utils/asyncHandeler.js
@@ -1,12 +1,20 @@
-const asyncHandeler = (fn) => { async (req, res) => {
-    try{
-        await fn(req, res, next)
-    }catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        })
-    }
-} }
+// const asyncHandeler = (fn) => { async (req, res) => {
+//     try{
+//         await fn(req, res, next)
+//     }catch (error) {
+//             res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// } }
 
-export {asyncHandeler}
+// export {asyncHandeler}
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch
+    ((err) => next(err))
+    }
+}
+export { asyncHandler }
